1122d3c99ff62c1c9a113b6509cc45e6eb9276ee
Jameka Echols <jameka.echols@gmail.com>
2021-06-08 23:27:46 -0500

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Jameka Echols on 6/8/21.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
